4e4773cd3bc1328fd1150c2fe5fba9368c00eae0
Rayshawn Woolard <r.woolard456@gmail.com>
2018-11-14 20:47:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,15 @@ function nowServing (katzDeliLine) {
     katzDeliLine.shift();
   }
   return ("There is nobody waiting to be served!");
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return ("The line is currently empty.")
   }
+  var string =  ["The line is currently: "]
+  for (var counter = 0; counter < katzDeliLine.length; counter++) {
+    string += counter + 1 + `. ${katzDeliLine[counter]}, `
+      }
+    return string
+}
