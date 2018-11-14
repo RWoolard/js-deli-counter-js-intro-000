906e03991756ed939b768188a1c3495cbb874686
Rayshawn Woolard <r.woolard456@gmail.com>
2018-11-14 20:30:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var katzDeliLine = [];
+
+function takeANumber (katzDeliLine, name) {
+katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+
+function nowServing (katzDeliLine) {
+     while (katzDeliLine.length > 0) {
+    return (`Currently serving ${katzDeliLine[0]}.`)
+  }
+  while(katzDeliLine.length > 0) {
+    katzDeliLine.shift();
+  }
+  return ("There is nobody waiting to be served!");
+  }
